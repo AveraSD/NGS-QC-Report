e1bfae456649a02a08e0eae0d3a1f4bd315e9b18
meissner.t <meissner.t@googlemail.com>
2015-01-15 18:49:19 -0800

    moved input files to the create report script, eventually they can be made command line arguments if needed

--- createReport.R
+++ createReport.R
@@ -9,12 +9,19 @@
 # MIT
 #
 ########################################
-args <- commandArgs(TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 
-rootDir <- args[0]
-sampleID <- args[1]
-sampleMeta <- args[2]
+rootDir <- args[1]
+sampleID <- args[2]
+sampleMeta <- args[3]
 
-print(args)
+# Input files >> can be made commandline arguments if needed..
+file1 <- paste(rootDir,'QC/',sampleID,'_1/',sampleID,'_1_fastqc.zip',sep='')
+file2 <- paste(rootDir,'QC/',sampleID,'_2/',sampleID,'_2_fastqc.zip',sep='')
+fileStar <- paste(rootDir,'star/',sampleID,'/Log.final.out',sep='')
+file3 <- paste(rootDir,'QC/',sampleID,'/rnaseqmetrics',sep='')
+file4 <- paste(rootDir,'QC/',sampleID,'/insertSize.txt',sep='')
+fileHT <- paste(rootDir,'counts/',sampleID,'_counts.txt',sep='')
+
+# create tex file
 Sweave('rnaseqqc_clia.Rnw')
-
