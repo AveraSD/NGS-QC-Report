8ad15d8d44e85accb78685b915872d325c6cee16
meissnert <meissner.t@googlemail.com>
2015-01-20 10:26:25 -0800

    added CallableLoci output to the report

--- createReport.R
+++ createReport.R
@@ -22,6 +22,7 @@ fileStar <- paste(rootDir,'star/',sampleID,'/Log.final.out',sep='')
 file3 <- paste(rootDir,'QC/',sampleID,'/rnaseqmetrics',sep='')
 file4 <- paste(rootDir,'QC/',sampleID,'/insertSize.txt',sep='')
 fileHT <- paste(rootDir,'counts/',sampleID,'_counts.txt',sep='')
+fileCLoci <- paste(rootDir, 'variants/', sampleID, '/callable_loci.txt', sep='')
 
 # create tex file
 Sweave('rnaseqqc_clia.Rnw')
